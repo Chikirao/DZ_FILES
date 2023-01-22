a7ac817995e2dfddf48d1ccb362f1a17f3d3bce5
Chikirao <ivankonanch@gmail.com>
2023-01-22 23:15:48 +0300

    userfriendly update

--- num2/num3.py
+++ num2/num3.py
@@ -1,6 +1,6 @@
 import os
 
-def readfile(filename):
+def readfile(filename): # Превращаем инфу с файла в нужную нам
      with open(filename, 'r', encoding='utf-8') as txt:
           textname = os.path.basename(txt.name)
           linescount = 0
@@ -10,8 +10,12 @@ def readfile(filename):
           data = txt.read()
      return f'{textname}\n{linescount}\n{data}\n'
 
-outtext = open('num2/texts_for_2/out.txt', 'w', encoding='utf-8')
+outtext = open('num2/out.txt', 'w', encoding='utf-8')
+
+# Запишем в выходной файл нужный текст
 
 outtext.write(readfile('num2/texts_for_2/1.txt'))
 outtext.write(readfile('num2/texts_for_2/2.txt'))
 outtext.write(readfile('num2/texts_for_2/3.txt'))
+
+outtext.close # Фенит!
